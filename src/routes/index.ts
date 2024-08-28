87746b428627c51fbc7c2971196224052495a4af
hungpn <phamnanghung.25@gmail.com>
2024-08-28 21:16:59 +0700

    test cicd

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,9 @@ import { Router } from 'express';
 import routeRSVP from './rsvp';
 
 const route = Router();
-
+route.get('/', (req, res) => {
+  res.json({ success: true });
+});
 route.use('/', routeRSVP);
 
 export default route;
