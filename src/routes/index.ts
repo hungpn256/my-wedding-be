2cc28aed3dc0a5f47961ce46a0a77cb314f5c2bb
HungPN <hungpn@HungPN-D8-M.local>
2024-10-11 13:47:04 +0700

    search ytb

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import routeRSVP from './rsvp';
+import searchRoute from './search';
 
 const route = Router();
 route.get('/', (req, res) => {
   res.json({ success: true });
 });
 route.use('/', routeRSVP);
+route.use('/', searchRoute);
 
 export default route;
