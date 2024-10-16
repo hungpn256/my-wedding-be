96537134b63760e24d1938cfd15b7975bfe522cb
hungpn <phamnanghung.25@gmail.com>
2024-10-16 20:34:15 +0700

    ok

--- src/routes/rsvp.ts
+++ src/routes/rsvp.ts
@@ -6,10 +6,11 @@ const routeRSVP = Router();
 
 routeRSVP.get('/rsvp', getRSVPController);
 routeRSVP.post('/rsvp', createRSVPController);
-routeRSVP.get('/rsvp/:rsvpId', (req, res) => {
+routeRSVP.get('/rsvp/:rsvpId', async (req, res) => {
   try {
     const id = parseInt(req.params.rsvpId);
-    prisma.rSVP.update({ where: { id }, data: { isAproved: true } });
+    await prisma.rSVP.update({ where: { id }, data: { isAproved: true } });
+    res.json({ success: true });
   } catch (error: any) {
     res.status(400).json({ message: error.message });
   }
